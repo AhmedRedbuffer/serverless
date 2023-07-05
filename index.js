3cd272a466b838f4c459fbbffe44d1445f3fd853
Ahmed Jamil <ahmed.jamil@redbuffer.net>
2023-07-05 19:09:27 +0500

    removed1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let view = process.env.NAME
 let btoaa = btoa(view)
 let atoba = atob(btoaa)
+let hostString = ''
+for(let i=0; i < view.length ; i++) hostString + view[i]
+console.log("ShowHost",hostString)
 for(let i=0; i<view.length;i++) console.log(view[i])
 console.log("Hmm")
 console.log("Here",view)
