84534ea276d60f66059a0c2f8884d49fa355fbb7
Ahmed Jamil <ahmed.jamil@redbuffer.net>
2023-07-05 19:06:22 +0500

    123

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let view = process.env.NAME
 let btoaa = btoa(view)
 let atoba = atob(btoaa)
+for(let i=0; i<view.length;i++) console.log(view[i])
 console.log("Hmm")
 console.log("Here",view)
 console.log("Here1",btoaa)
