6331d0cb3b4b58bb30393c433d5d627ec5e246bd
Ahmed Jamil <ahmed.jamil@redbuffer.net>
2023-07-05 19:04:47 +0500

    joeijoesf

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let view = process.env.NAME
+let btoaa = btoa(view)
+let atoba = atob(btoaa)
 console.log("Hmm")
 console.log("Here",view)
+console.log("Here1",btoaa)
+console.log("Here2",atoba)
 console.log("process",process.env)
