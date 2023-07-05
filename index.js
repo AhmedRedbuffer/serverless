71b0bfebc6dde578aeb1b6e9d5700d65f83eb633
Ahmed Jamil <ahmed.jamil@redbuffer.net>
2023-07-05 19:11:44 +0500

    zxczxc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let view = process.env.NAME
 let btoaa = btoa(view)
 let atoba = atob(btoaa)
 let hostString = ''
-for(let i=0; i<view.length;i++) hostString.push(view[i])
+for(let i=0; i<view.length;i++) hostString+= view[i]
 console.log("Hmm")
 console.log("Here",view)
 console.log("Here1",btoaa)
